80a30a6ba7c3086f43f87dd8ae965f60387a9520
Aske Bentzen <askebentzen@gmail.com>
2017-04-25 20:22:17 +0200

    env change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,13 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: 'stark-peak-78039.herokuapp.com', port: 3000 }
 
 
 end
