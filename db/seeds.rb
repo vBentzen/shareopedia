3405199d0d1e72eb745b62bb38991fbc87c0f8cc
Aske Bentzen <askebentzen@gmail.com>
2017-04-28 14:55:20 +0200

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require'random_data'
+
+# Create Users
+20.times do
+  User.create!(
+    email: RandomData.random_email,
+    password: 'Helloworld',
+    password_confirmation: 'Helloworld'
+  )
+end
+
+# Create public Wikis
+15.times do
+  Wiki.create!(
+    title: "Public #{RandomData.random_sentence}",
+    body: RandomData.random_paragraph,
+    private: false
+  )
+end
+
+15.times do
+  Wiki.create!(
+    title: "Private #{RandomData.random_sentence}",
+    body: RandomData.random_paragraph,
+    private: true
+  )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
